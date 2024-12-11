abc4031d7846c006c6fd0edc0b940479c8484640
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-12-11 09:52:13 +0530

    attach the kb sample

---
+++ CustomizeAnnotationArrow/CustomizeAnnotationArrow/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomizeAnnotationArrow
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
